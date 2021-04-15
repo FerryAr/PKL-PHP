0b799fcd6b4b03edcc14190e052035ce8f12c62a
Azriel Akbar <ferryakbarardiansyah@gmail.com>
2021-04-16 06:12:30 +0700

    delete unused file

--- app/koneksi.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $koneksi = mysqli_connect("HOST-MYSQL", "USERNAME-MYSQL", "PASS-MYSQL", "NAMA_DATABASE");
-    //if(mysqli_connect_errorno()) {
-    //    echo "Koneksi ke database error! Dengan log : " . mysqli_connect_error();
-    //}
-?>
